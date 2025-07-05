3053fb567a6f34b59c453cefc3d7d6b938dd7160
francisco-leal <francisco@talentprotocol.com>
2025-07-05 12:27:21 +0200

    Add uniswap as an iframe

---
+++ lib/constants.ts
@@ -0,0 +1,21 @@
+import { Token, ChainId } from "@uniswap/sdk-core";
+
+export const QUOTER_CONTRACT_ADDRESS =
+  "0x0d5e0f971ed27fbff6c2837bf31316121532048d";
+const TALENT_TOKEN_ADDRESS = process.env.NEXT_PUBLIC_LIQUIDITY_TOKEN_ADDRESS!;
+
+export const ETH_TOKEN = new Token(
+  ChainId.BASE,
+  "0x0000000000000000000000000000000000000000",
+  18,
+  "ETH",
+  "Ether"
+);
+
+export const TALENT_TOKEN = new Token(
+  ChainId.BASE,
+  TALENT_TOKEN_ADDRESS,
+  18,
+  "TALENT",
+  "TalentProtocolToken"
+);
